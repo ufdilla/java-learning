cb3ee74bbb54fb5bbd3b92bcee58d980696c0719
ufdilla <learnmore.cumi@gmail.com>
2017-12-06 11:02:24 +0700

    nampilin hello world, bikin objek, bikin kelas, assign variabel, panggil construktor employee dari file berbeda, tipe data

---
+++ datatype.java
@@ -0,0 +1,16 @@
+/**
+ * datatype
+ */
+public class datatype {
+
+    public void usia() {
+        int usia = 4;
+        usia = usia + 3;
+        System.out.println("the age is : " + usia);
+    }
+
+    public static void main(String[] args) {
+        datatype test = new datatype();
+        test.usia();
+    }
+}
